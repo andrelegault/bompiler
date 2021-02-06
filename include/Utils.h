6efe3e8c3d75d51cb0696565ea4946494f2f82f6
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2021-02-06 07:18:01 -0500

    feat: add string split util

---
+++ include/Utils.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <vector>
+#include <string>
+
+namespace Utils {
+    std::vector<std::string> split_string(const std::string& str, const std::string &delim);
+}
---
+++ src/Utils.cpp
@@ -0,0 +1,18 @@
+#include "Utils.h"
+
+// from https://stackoverflow.com/a/57346888/7705786
+std::vector<std::string> Utils::split_string(const std::string& i_str, const std::string& i_delim) {
+    std::vector<std::string> result;
+
+    size_t found = i_str.find(i_delim);
+    size_t startIndex = 0;
+
+    while (found != std::string::npos) {
+        result.push_back(std::string(i_str.begin() + startIndex, i_str.begin() + found));
+        startIndex = found + i_delim.size();
+        found = i_str.find(i_delim, startIndex);
+    }
+    if (startIndex != i_str.size())
+        result.push_back(std::string(i_str.begin() + startIndex, i_str.end()));
+    return result;
+}
