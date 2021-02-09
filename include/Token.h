ecb824ec8152547b68e379f04a0d367c548e93b3
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2021-02-08 21:50:25 -0500

    refactor: set default param values for constructor

--- include/Token.h
+++ include/Token.h
@@ -4,7 +4,7 @@
 
 class Token {
 public:
-    Token(const std::string& type, const std::string& lexeme, const int& line);
+    Token(const std::string& type = "undefined", const std::string& lexeme = "undefined", const int& line = -1);
     friend std::ostream& operator<<(std::ostream&, const Token& token);
 private:
     const std::string type;
