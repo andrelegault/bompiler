efda28d8aa9a132d2b2015dd2539742cd1548f57
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2021-03-07 08:27:54 -0500

    refactor: make token members public

--- include/Token.h
+++ include/Token.h
@@ -8,7 +8,6 @@ public:
     Token(const std::string& type = "undefined", const std::string& lexeme = "undefined", const int& line = -1);
     friend std::ostream& operator<<(std::ostream&, const Token& token);
     virtual void process(std::ofstream& out_tokens, std::ofstream& out_errors) const;
-protected:
     const std::string type;
     const std::string lexeme;
     const int line;
