4816e78a2dcfc41fb62dd0696f91dd2d36f022ba
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2021-03-11 04:32:28 -0500

    feat: add semantic action boilerplate

--- src/AST.cpp
+++ src/AST.cpp
@@ -6,7 +6,58 @@
 
 #include "AST.h"
 
-struct ASTNode {
-	ASTNode *parent, *leftmost_child, *leftmost_sibling, *next;
-	ASTNode(ASTNode *parent, ASTNode *leftmost_child, ASTNode *leftmost_sibling, ASTNode *next);
-};
+#include <string>
+
+using std::string;
+
+ASTNode::ASTNode() {}
+
+ASTNode::ASTNode(string &value) : value(value) {}
+
+ASTNode *ASTNode::make_siblings(ASTNode *y) {
+    ASTNode *xsibs = this;
+    while (xsibs->right != nullptr) {
+        xsibs = xsibs->right;
+    }
+
+    ASTNode *ysibs = y->leftmost_sibling;
+    xsibs->right = ysibs;
+
+    ysibs->leftmost_sibling = xsibs->leftmost_sibling;
+    ysibs->parent = xsibs->parent;
+
+    while (ysibs->right != nullptr) {
+        ysibs = ysibs->right;
+        ysibs->leftmost_sibling = xsibs->leftmost_sibling;
+        ysibs->parent = xsibs->parent;
+    }
+
+    return ysibs;
+}
+
+void ASTNode::adopt_children(ASTNode *y) {
+    if (this->leftmost_sibling != nullptr) {
+        this->leftmost_sibling->make_siblings(y);
+    } else {
+        ASTNode *ysibs = y->leftmost_sibling;
+        this->leftmost_sibling = ysibs;
+        while (ysibs != nullptr) {
+            ysibs->parent = this;
+            ysibs = ysibs->right;
+        }
+    }
+}
+
+ASTNode *ASTNode::make_family(string op, ASTNode *kid1, ASTNode *kid2) {
+    ASTNode *node = ASTNode::make_node(op);
+    node->adopt_children(kid1->make_siblings(kid2));
+    return node;
+}
+
+ASTNode *ASTNode::make_node() {
+    return new ASTNode();
+}
+
+ASTNode *ASTNode::make_node(string type) {
+    return new ASTNode(type);
+}
